4333951003fe2b633ffa156c0714d199c971a7e9
Manel <manelscardoso_94@hotmail.com>
2025-08-01 17:39:54 +0100

    Client and Channel classes implemented

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,46 +1,127 @@
 #include "../includes/Channel.hpp"
 
-Channel::Channel(std::string str){
-    _name = str;
-    _key = false;
-    _invite = false;
+Channel::Channel()
+    : _key(false), _invite(false), _topicRestricted(false), _userLimit(0)
+{
 }
 
-Channel::~Channel(){}
-
-bool    Channel::getKey() const{
-    return _key;
+Channel::Channel(const std::string &name)
+    : _name(name), _key(false), _invite(false), _topicRestricted(false),
+      _userLimit(0)
+{
 }
 
-bool    Channel::getInvite() const{
-    return _invite;
-}
-
-std::string Channel::getName() const{
-    return _name;
-}
-
-std::string Channel::getTopic() const{
-    return _topic;
-}
-
-Channel::Channel(){}
-
-Channel::Channel(const Channel& other){
+Channel::Channel(const Channel &other)
+{
     _name = other._name;
     _topic = other._topic;
     _key = other._key;
+    _keyValue = other._keyValue;
     _invite = other._invite;
+    _topicRestricted = other._topicRestricted;
+    _userLimit = other._userLimit;
+    _members = other._members;
+    _operators = other._operators;
+    _invited = other._invited;
 }
 
-Channel& Channel::operator=(const Channel& other){
+Channel    &Channel::operator=(const Channel &other)
+{
     if (this != &other)
     {
         _name = other._name;
         _topic = other._topic;
         _key = other._key;
+        _keyValue = other._keyValue;
         _invite = other._invite;
+        _topicRestricted = other._topicRestricted;
+        _userLimit = other._userLimit;
+        _members = other._members;
+        _operators = other._operators;
+        _invited = other._invited;
     }
-    return *this;
 }
-       
+
+Channel::~Channel()
+{
+}
+
+std::string    Channel::getName() const
+{
+    return (_name);
+}
+
+std::string    Channel::getTopic() const
+{
+    return (_topic);
+}
+
+bool   Channel::getKey() const
+{
+    return (_key);
+}
+
+std::string    Channel::getKeyValue() const
+{
+    return (_keyValue);
+}
+
+bool    Channel::getInvite() const
+{
+    return (_invite);
+}
+
+bool    Channel::getTopicRestricted() const
+{
+    return (_topicRestricted);
+}
+
+int Channel::getUserLimit() const
+{
+    return (_userLimit);
+}
+
+const std::vector<std::string>& Channel::getMembers() const
+{
+    return (_members);
+}
+
+const std::vector<std::string>& Channel::getOperators() const
+{
+    return (_operators);
+}
+
+const std::vector<std::string>& Channel::getInvited() const
+{
+    return (_invited);
+}
+
+bool    Channel::addMember(const std::string& nick)
+{
+    if (isMember(nick))
+    {
+        _members.push_back(nick);
+        return (true);
+    }
+    return (false);
+}
+
+bool    Channel::removeMember(const std::string& nick)
+{
+    std::vector<std::string>::iterator  it = std::find(_members.begin(), _members.end(), nick);
+
+    if (it != _members.end())
+    {
+        _members.erase(it);
+        removeOperator(nick);
+        return (true);
+    }
+    return (false);
+}
+
+bool    Channel::isMember(const std::string& nick) const
+{
+    return (std::find(_members.begin(), _members.end(), nick) != _members.end());
+}
+
+
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,29 +1,81 @@
 #include "../includes/Client.hpp"
 
+Client::Client() : fd(-1), registered(false) {}
 
-Client::Client(){
-}
+Client::Client(int fd) : fd(fd), registered(false) {}
 
-Client::~Client(){
-
-}
-
-std::string Client::getClient() const{
-    return client;
-}
-
-std::string Client::getNick() const{
-    return nick;
-}
-
-Client::Client(const Client& other){
+Client::Client(const Client &other) {
     client = other.client;
+    username = other.username;
+    nick = other.nick;
+    pass = other.pass;
+    fd = other.fd;
+    registered = other.registered;
+    operatorStatus = other.operatorStatus;
+    channels = other.channels;
 }
 
-Client& Client::operator=(const Client& other){
-    if (this != &other)
-    {
+Client &Client::operator=(const Client &other) {
+    if (this != &other) {
         client = other.client;
+        username = other.username;
+        nick = other.nick;
+        pass = other.pass;
+        fd = other.fd;
+        registered = other.registered;
+        operatorStatus = other.operatorStatus;
+        channels = other.channels;
     }
-    return *this;
+    return (*this);
+}
+
+Client::~Client() {}
+
+std::string Client::getClient() const { return (client); }
+
+std::string Client::getUsername() const { return (username); }
+
+std::string Client::getNickname() const { return (nick); }
+
+int Client::getFd() const { return (fd); }
+
+bool Client::isRegistered() const { return (registered); }
+
+bool Client::isOperator(const std::string &channel) const {
+    std::map<std::string, bool>::const_iterator it =
+        operatorStatus.find(channel);
+    return (it != operatorStatus.end() && it->second);
+}
+
+const std::vector<std::string> &Client::getChannels() const {
+    return (channels);
+}
+
+void Client::setClient(const std::string &client) { this->client = client; }
+
+void Client::setUsername(const std::string &username) {
+    this->username = username;
+}
+
+void Client::setNickname(const std::string &nick) { this->nick = nick; }
+
+void Client::setRegistered(bool status) { registered = status; }
+
+void Client::setOperator(const std::string &channel, bool status) {
+    operatorStatus[channel] = status;
+}
+
+void Client::addChannel(const std::string &channel) {
+    if (std::find(channel.begin(), channel.end(), channel) == channels.end())
+        channels.push_back(channel);
+}
+
+void Client::removeChannel(const std::string &channel) {
+    std::vector<std::string>::iterator it =
+        std::find(channels.begin(), channels.end(), channel);
+
+    if (it != channels.end()) {
+        channels.erase(it);
+        operatorStatus.erase(channel);
+    }
 }
