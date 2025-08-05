bdaef7d26ef3df348e8285277385d27fbf47e18c
Manel <manelscardoso_94@hotmail.com>
2025-08-05 14:59:07 +0100

    handleJoin done and handleKick in the works

--- obj/Reply.d
+++
@@ -1,4 +0,0 @@
-obj/Reply.o: src/Reply.cpp src/../includes/Reply.hpp \
- src/../includes/Client.hpp src/../includes/Server.hpp \
- src/../includes/Input.hpp src/../includes/Channel.hpp \
- src/../includes/Reply.hpp src/../includes/Errors.hpp
--- obj/Server.d
+++
@@ -1,5 +0,0 @@
-obj/Server.o: src/Server.cpp src/../includes/Server.hpp \
- src/../includes/Client.hpp src/../includes/Input.hpp \
- src/../includes/Channel.hpp src/../includes/Reply.hpp \
- src/../includes/Server.hpp src/../includes/Errors.hpp \
- src/../includes/Input.hpp src/../includes/Reply.hpp
