cdfb1b57c144577d26e27b6ea1953877df385200
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2025-08-07 16:34:29 +0100

    Input.hpp luigi_branch

--- includes/Input.hpp
+++ includes/Input.hpp
@@ -1,26 +1,58 @@
-#ifndef INPUT_HPP
-#define INPUT_HPP
+#pragma once
 
-#include <iostream>
-#include <vector>
 #include <sstream>
+#include <string>
+#include <iostream>
+#include <stdexcept>
 
+class Input {
 
-class Input{
-    public:
-        Input(std::string);
-        Input& operator=(const Input&);
-        ~Input();
-        Input();
+private:
+    void parseComponents();
+    void validateBaseCommand();
 
-        std::string getCommand() const;
+    std::string _raw;
+    std::string _prefix;
+    std::string _command;
+    std::string _nick;
+    std::string _channel;
+    std::string _target;
+    std::string _key;
+    std::string _comment;
+	std::string _type;
+	std::string _pass;
+    std::string _trailing;
 
-    private:
-        Input(const Input&);
-        
+    bool		_isValid;
 
-        std::string                 _command;
-        std::string                 _inClient;
-        std::vector<std::string>    _args;
+public:
+    Input();
+    ~Input();
+
+    //PARSE BASES
+    void	parse(const std::string &complete_message);
+    void	clear();
+
+	//PARSE COMMANDS
+	void	parseJoin(std::istringstream &iss);
+	void	parseInvite(std::istringstream &iss);
+	void	parseKick(std::istringstream &iss);
+	void	parseMode(std::istringstream &iss);
+	void	parsePass(std::istringstream &iss);
+	void	parseNick(std::istringstream &iss);
+	void	parseUser(std::istringstream &iss);
+	void	parsePart(std::istringstream &iss);
+	void	parsePrivmsg(std::istringstream &iss);
+	void	parseTopic(std::istringstream &iss);
+
+    //GETTERS
+    const std::string	&getPrefix() const;
+    const std::string	&getCommand() const;
+	const std::string	&getTrailing() const;
+    bool				isValid() const;
+    const std::string	&getRaw() const;
+
+	//Verificacao de mensagens parciais
+	bool isComplete(const std::string &message) const;
+
 };
-#endif
