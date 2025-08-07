2d7d5bb401847917fb81d84eaa2849952c1fab91
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2025-08-07 16:32:27 +0100

    Client.cpp LUIGI_BRANCH

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,29 +1,66 @@
-#include "../includes/Client.hpp"
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
 
+# include <iostream>
+# include <map>
+# include <vector>
+# include <algorithm>
+# include "../includes/Input.hpp"
 
-Client::Client(){
-}
+class Client
+{
+private:
+	std::string	client;
+	std::string	username;
+	std::string	nick;
+	std::string	pass;
+	std::string _partialMessage;
+    std::string _buffer;
 
-Client::~Client(){
+	Input		_input;
 
-}
+	int		fd;
+	bool	registered;
 
-std::string Client::getClient() const{
-    return client;
-}
+	std::map<std::string, bool>	operatorStatus;
+	std::vector<std::string>	channels;
 
-std::string Client::getNick() const{
-    return nick;
-}
+public:
+	Client();
+	Client(int fd);
+	Client& operator=(const Client& other);
+	Client(const Client&);
+	~Client();
 
-Client::Client(const Client& other){
-    client = other.client;
-}
+	//INPUT
+	Input	&getInput() { return _input; }
+    const Input	&getInput() const { return _input; }
+	void	appendPartialMessage(const std::string &message);
+	void	clearInput();
+	bool	processMessage(const std::string &message);
+	bool	processInitialCommands();
 
-Client& Client::operator=(const Client& other){
-    if (this != &other)
-    {
-        client = other.client;
-    }
-    return *this;
-}
+	//Getters
+	std::string	getClient() const;
+	std::string	getUsername() const;
+	std::string	getNickname() const;
+
+	int			getFd() const;
+
+	bool		isRegistered() const;
+	bool		isOperator(const std::string& channel) const;
+
+	const std::vector<std::string>&	getChannels() const;
+
+	//Setters
+	void	setClient(const std::string& client);
+	void	setUsername(const std::string& username);
+	void	setNickname(const std::string& nick);
+	void	setRegistered(bool status);
+	void	setOperator(const std::string& channel, bool status);
+
+	void	addChannel(const std::string& channel);
+	void	removeChannel(const std::string& channel);
+};
+
+#endif
