0acfa49d9a7fcb18d234c3c37e23c1845cfc0c59
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2025-08-07 16:35:24 +0100

    Update Server.hpp

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -3,6 +3,7 @@
 
 # include <iostream>
 # include <vector>
+# include <map>
 # include <ctime>
 # include <cstdlib>
 # include <poll.h>
@@ -10,61 +11,76 @@
 # include <netinet/in.h>
 # include <sys/socket.h>
 # include <fcntl.h>
-#include <cstring>
+# include <errno.h>
+# include <cstring>
 
 
 # include "Client.hpp"
 # include "Input.hpp"
 # include "Channel.hpp"
+# include "Reply.hpp"
+# include "Errors.hpp"
 
 class Server{
     public:
         Server(std::string, std::string);
         ~Server();
-        
+
         std::string getName() const;
         std::string getVersion() const;
         std::string getStTime() const;
 
-        void    start();
-        void    run();
-        void    newClient();	
-	    void    clientRequest(int);
-        void    executeCommand();
+        void        start();
+        void        run();
+        void        newClient();
+        void        clientRequest(int);
+        void        executeCommand(int);
+        void        process_login();
 
-        void    handleInvite();
-        void    handleJoin();
-        void    handleKick();
-        void    handleMode();
-        void    handleNick();
-        void    handlePart();
-        void    handlePass();
-        void    handleTopic();
-        void    handlePrivmsg();
-        void    handleUser();
-        
-        
+		void		processRegister(Client *client);
+
+        void        handleInvite(int);
+        void        handleJoin(int);
+        void        handleKick(int);
+        void        handleMode(int);
+        void        handleNick(int);
+        void        handlePart(int);
+        void        handlePass(int);
+        void        handleTopic(int);
+        void        handlePrivmsg(int);
+        void        handleUser(int);
+
+        static Server*          instance;
+        void        closeExit();
+
     private:
         Server();
         Server(const Server&);
         Server& operator=(const Server&);
 
-        std::vector<Client>     _clients;
-        std::vector<Channel>    _channels;
-        Input                   _input;
-        std::string             _network_name;
-        std::string             _version;
-        std::string             _StartTime;
-        uint16_t                _port;
-        std::string             _password;
-        int                     _nbClients;
-        struct pollfd			_fds[1024];
-        int                     _socketfd;
+        std::map<std::string, Client>   _clients;
+        std::map<std::string, Channel>  _channels;
+        // Input                           _input;
+        std::string                     _network_name;
+        std::string                     _version;
+        std::string                     _StartTime;
+        uint16_t                        _port;
+        std::string                     _password;
+        int                             _nbClients;
+        struct pollfd			        _fds[1024];
+        int                             _socketfd;
 
-        void    setDateTime();
-        void    parsePort(std::string);
+        void        setDateTime();
+        void        parsePort(std::string);
+        void        joinGreetings(int);
+
+        Client*     findClientByFd(int fd);
+        Client*     findClientByNick(const std::string& nick);
+        Channel*    findChannel(const std::string& name);
+        void        sendMessage(int fd, const std::string& message);
 };
+extern Server* instance;
 
-void signal_ignore();
-
+void signalIgnore();
+void handleSigint(int);
 # endif
